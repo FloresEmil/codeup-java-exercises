710a39f9a6d0585178504e8bd692dbbeb9d6f293
Emil Flores <emil.alejandro.flores@gmail.com>
2022-11-09 09:23:12 -0600

    exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,28 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println();
+
+        int myFavoriteNumber = 14;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "3.14159";
+        System.out.println(myString);
+
+        float myNumber = (float) 3.14;
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+//        int class =
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
     }
 }
