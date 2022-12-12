b67d15f532373287a833f8897f74505dcf6e0468
Emil Flores <emil.alejandro.flores@gmail.com>
2022-12-12 10:26:02 -0600

    while, do while and for loop done

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,9 +1,44 @@
+import java.util.Arrays;
+
 public class ArraysExercises {
 
-    int[] numbers = {1, 2, 3, 4, 5};
-System.out.println(numbers);
+//    public static Person[] addPerson(Person[] people, Person person) {
+//
+//        Person[] newArray = new Person[People.lenght
+//                +1];
+//        System.arraycopy(people, 0, newArray, 0, people.lenght);
+//        newArray[people.leght] = person;
+//        return newArray;
+//    }
 
 
+
+        public static void main(String[] args) {
+            int[] numbers = {1, 2, 3, 4, 5};
+            System.out.println(Arrays.toString(numbers));
+
+            Person pim = new Person("pim");
+            Person pam = new Person("pam");
+            Person pum = new Person("pum");
+
+
+            Person[] people = {pim, pam, pum};
+
+            for (Person person : people) {
+                System.out.println(person.getName());
+            }
+
+            addPerson(people, "");
+
+        }
+
+    public static void addPerson(Person[] people, String name) {
+        Person[] personCopy = Arrays.copyOf(people, people.length + 1);
+        personCopy[personCopy.length - 1] = new Person(name);
+        for (Person person : personCopy) {
+            System.out.println(person.getName());
+        }
+    }
 
 
 
--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,2 +1,19 @@
+
+
 public class StringExercise {
+    public static void main(String[] args){
+
+        String string1 = "We don't need no education.\nWe don't need no thought control";
+        System.out.println(string1);
+
+        String string2 = "Check \"this\" out!, \"s inside of \"s!\n";
+        System.out.println(string2);
+
+        String string3 = "In windows, the main drive is usually C:\\\n";
+        System.out.println(string3);
+
+        String string4 = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(string4);
+    }
 }
